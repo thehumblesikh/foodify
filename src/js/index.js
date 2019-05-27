828d73aecdd0602ee9e96256e22ddb34c8606be7
jazdosanjh <jas@dosanjh.co.uk>
2019-05-27 17:30:29 +0100

    Completed 50% of the search view which now shows search results. Also created base.js file for all the DOM elements to improve overall project structure.

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,6 @@
 import Search from './models/Search';
+import * as searchView from './views/searchView';
+import { elements } from './views/base';
 
 /** Global State of the app
  * - Search object
@@ -11,25 +13,25 @@ const state = {};
 
 const controlSearch = async () => {
     // 1. Get query from view
-    const query = 'pizza'; //TODO
+    const query = searchView.getInput();
+    console.log(query);
 
     if (query) {
         // 2. New search object and add to state
         state.search = new Search(query);
 
         // 3. Prepare UI for results
-
+        searchView.clearInput();
+        searchView.clearResults();
         // 4. Search for recipes
         await state.search.getResults();
 
         // 5. Render results on UI
-        console.log(state.search.result);
+        searchView.renderResults(state.search.result);
     }
 }
 
-document.querySelector('.search').addEventListener('submit', e => {
+elements.searchForm.addEventListener('submit', e => {
     e.preventDefault();
     controlSearch();
 });
-
-search.getResults();
