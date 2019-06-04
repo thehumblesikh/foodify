f626603c9df8a2d3d8a12e93a4cdf4506cb79317
jazdosanjh <jas@dosanjh.co.uk>
2019-06-04 11:58:41 +0100

    Started work on the recipe view for the app. The recipes are now displaying in the hero section of the app.

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import Search from './models/Search';
 import Recipe from './models/Recipe';
 import * as searchView from './views/searchView';
+import * as recipeView from './views/recipeView';
 import { elements, renderLoader, clearLoader } from './views/base';
 import { get } from 'http';
 
@@ -65,6 +66,8 @@ const controlRecipe = async () => {
 
     if (id) {
         // Prepare UI for changes
+        recipeView.clearRecipe();
+        renderLoader(elements.recipe);
 
         // Create new recipe object
         state.recipe = new Recipe(id);
@@ -79,7 +82,9 @@ const controlRecipe = async () => {
             state.recipe.calcServings();
 
             // Render recipe
-            console.log(state.recipe);
+            clearLoader();
+            recipeView.renderRecipe(state.recipe);
+
         } catch (err) {
             alert('Error processing recipe!');
         }
